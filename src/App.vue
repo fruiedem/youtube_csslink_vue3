f732da668ff54b3f20780b9cb91dfe8857f8afd5
changjunkim <changjunkim@lotte.net>
2025-04-18 17:47:49 +0900

    영화별 모달창 띄우기, 닫기 기능

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,113 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>영화정보</h1>
+  <div v-for="(movie, i) in data" :key="i" class="item">
+    <figure>
+      <img :src="`${movie.imgUrl}`" :alt="movie.title">
+    </figure>
+    <div class="info">
+      <h3 class="bg-yellow" >{{ movie.title }}</h3>
+      <p>개봉 : {{ movie.year }} </p>
+      <p>장르 : {{ movie.category }}</p>
+      <button @:click="increaseLike(i)">좋아요 </button>
+      <span>{{ movie.like }}</span>
+      <p>
+        <button @:click="isModal=true; selectedMovie=i">상세보기</button>
+      </p>
+    </div>
+
+  </div>
+  <!-- 모달창 추가 -->
+  <div class="modal" v-if="isModal">
+    <div class="inner">
+      <h3>{{ data[selectedMovie].title }}</h3>
+      <p>영화 상세정보</p>
+      <p> {{ data[selectedMovie].details }}</p>
+      <button @:click="isModal=false">닫기</button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import data from './assets/movies';
+console.log(data);
+  export default {
+    name: 'App',
+    data() {
+      return {
+        isModal: false,
+        data: data,
+        selectedMovie: 0
+      }
+    },
+    methods: {
+      increaseLike(i) {
+        this.data[i].like += 1;
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body {
+    max-width: 768px;
+    margin: 0 auto;
+    padding: 20px;
+  }
+
+  h1, h2, h3 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    margin-bottom: 0.5 rem;
+  }
+
+  button {
+    margin-right: 10px;
+    margin-top: 1rem;
+  }
+
+  .item {
+    width: 100%;
+    border: 1px solid #ccc;
+    display: flex;
+    margin-bottom: 20px;
+    padding: 1rem;
+  }
+  .item figure {
+    width: 30%;
+    margin-right: 1rem;
+  }
+
+  .item img {
+    width : 100%;
+  }
+  
+  .item .info {
+    width: 100%;
+  }
+
+  .modal {
+    background: rgba(0,0,0,0.7);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal .inner {
+    background: #fff;
+    width: 80%;
+    padding: 20px;
+    border-radius: 10px;
+  }
 </style>
---
+++ src/assets/movies.js
@@ -0,0 +1,31 @@
+const data = [
+          {
+            title: "노량",
+            year: 2023,
+            category: "액션, 드라마",
+            textColor: "color: red",
+            like: 0,
+            imgUrl: "./assets/노량.jfif",
+            details: "1598년 노량해협, 명량 해전 이후 도요토미 히데요시가 죽자 왜군은 퇴각을 준비한다. 이순신 장군은 명나라 장군 진린과 함께 7년간의 임진왜란을 끝낼 최후의 격전을 펼친다."
+          },
+          {
+            title: "승부",
+            year: 2025,
+            category: "드라마",
+            textColor: "color: blue",
+            like: 0,
+            imgUrl: "./assets/승부.jfif",
+            details: "대한민국 바둑계의 전설 조훈현과 그의 오랜 제자 이창호의 특별한 인연, 바둑 인생, 세기의 이목이 집중되었던 대국을 조명한다."
+          },
+          {
+            title: "야당",
+            year: 2025,
+            category: "범죄",
+            textColor: "color: green",
+            like: 0,
+            imgUrl: "./assets/야당.jfif",
+            details: "대한민국 마약판과 수사기관 사이를 오가며 마약 세계를 뒤흔드는 내부자 ‘야당’을 중심으로 펼쳐지는 이야기"
+          },
+        ]
+
+export default data;
