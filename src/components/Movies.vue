401e3e973d2bb3d60cea5c0057749f50b027893f
fruiedem <fruiedem@gmail.com>
2025-04-21 14:06:08 +0900

    영화목록 스타일링 추가

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,21 +1,23 @@
 <template>
-    <h1>영화정보</h1>
-  <div v-for="(movie, i) in data" :key="i" class="item">
-    <figure>
-      <img :src="`${movie.imgUrl}`" :alt="movie.title">
-    </figure>
-    <div class="info">
-      <h3 class="bg-yellow" >{{ movie.title }}</h3>
-      <p>개봉 : {{ movie.year }} </p>
-      <p>장르 : {{ movie.category }}</p>
-      <button @:click="$emit('increaseLike')">좋아요 </button>
-      <span>{{ movie.like }}</span>
-      <p>
-        <button @:click="isModal=true; selectedMovie=i">상세보기</button>
-      </p>
+  <div class="container">
+    <div v-for="(movie, i) in data" :key="i" class="item">
+      <figure>
+        <img :src="`${movie.imgUrl}`" :alt="movie.title">
+      </figure>
+      <div class="info">
+        <h3 class="bg-yellow" >{{ movie.title }}</h3>
+        <p>개봉 : {{ movie.year }} </p>
+        <p>장르 : {{ movie.category }}</p>
+        <button @:click="$emit('increaseLike')">좋아요 </button>
+        <span>{{ movie.like }}</span>
+        <p>
+          <button @:click="isModal=true; selectedMovie=i">상세보기</button>
+        </p>
+      </div>
     </div>
-    
   </div>
+  <h1>영화정보</h1>
+
 </template>
 <script>
 export default {
@@ -26,5 +28,7 @@ export default {
 }
 </script>
 <style >
-    
+    .container {
+      padding: 20px;
+    }
 </style>
